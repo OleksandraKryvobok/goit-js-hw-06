4826889d2b0169260689de64a1c723ceecb9fcef
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2021-12-05 19:54:40 +0200

    Добавляет правки в task 1

--- js/task-01.js
+++ js/task-01.js
@@ -4,12 +4,12 @@
 // найдет и выведет в консоль текст заголовка элемента
 // (тега < h2 >) и количество элементов в категории
 // (всех вложенных в него < li >).
-const categories = document.querySelectorAll('#categories > li');
-console.log(`Number of categories: ${categories.length}`);
+const categoriesEl = document.querySelectorAll('#categories > li');
+console.log(`Number of categories: ${categoriesEl.length}`);
 
-categories.forEach(category => {
-    const titleCategory = category.querySelector('h2');
-    console.log(`Category: ${titleCategory.textContent}`);
+categoriesEl.forEach(category => {
+    const title = category.querySelector('h2');
+    console.log(`Category: ${title.textContent}`);
     const item = category.querySelectorAll('li');
     console.log(`Elements: ${item.length}`);
 })

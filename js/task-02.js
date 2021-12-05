4ee3b97c5ed79884fcd02e3180851ff125a47b13
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2021-12-05 19:55:05 +0200

    Добавляет task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const items = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+});
+
+console.log(items);
+
+const ulEl = document.querySelector('ul');
+ulEl.append(...items);

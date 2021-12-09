98ec91ef81cd06f9a72d9a3aeea38e78f1f4779c
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2021-12-09 21:54:24 +0200

    Добавляет 9б 10 задачи

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  colorText: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+}
+
+const changeBackgroundColor = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorText.textContent = getRandomHexColor();
+}
+
+refs.btn.addEventListener('click', changeBackgroundColor);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  number: document.querySelector('[type="number"]'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+let amount;
+
+const changeAmount = (event) => {
+  const { value } = event.target;
+  amount = value;
+  console.log(value);
+  event.target.reset();
+}
+
+let x = 30;
+let div;
+
+const createBoxes = amount => {
+  for (let i = 1; i <= amount; i += 1) {
+    div = document.createElement('div');
+    div.style.height = `${x}px`;
+    div.style.width = `${x}px`;
+    x += 10;
+    div.style.backgroundColor = getRandomHexColor();
+    refs.boxes.append(div);
+  }
+}
+
+const destroyBoxes = () => {
+  refs.boxes.innerHTML = '';
+
+}
+
+refs.number.addEventListener('input', changeAmount);
+refs.createBtn.addEventListener('click', () => { createBoxes(amount) });
+refs.destroyBtn.addEventListener('click', destroyBoxes);
